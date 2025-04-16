6a285f9628645ddf493d8c7e7ed661e3e179d951
Max-MIT <maksudbek198501@gmail.com>
2025-04-17 06:43:11 +0900

    feat: develop apollo client connection

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,35 @@
+import { GET_PROPERTIES } from "@/apollo/user/query";
 import Advertisement from "@/libs/components/homepage/Advertisement";
 import PopularProperties from "@/libs/components/homepage/PopularProperties";
 import TopAgents from "@/libs/components/homepage/TopAgents";
 import TopProperties from "@/libs/components/homepage/TopProperties";
 import TrendProperties from "@/libs/components/homepage/TrendProperties";
 import withLayoutMain from "@/libs/components/layout/LayoutHome";
+import useDeviceDetect from "@/libs/hooks/useDeviceDetect";
+import { useQuery } from "@apollo/client";
 import { Stack } from "@mui/material";
 import { NextPage } from "next";
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
-import useDeviceDetect from "@/libs/hooks/useDeviceDetect";
 
 const Home: NextPage = () => {
   const device = useDeviceDetect();
+
+  const {
+    loading: getPropertiesLoading,
+    data: getPropertiesData,
+    error: getPropertiesErroor,
+    refetch: getPropertiesRefetch,
+  } = useQuery(GET_PROPERTIES, {
+    fetchPolicy: "network-only",
+    variables: {
+      input: {
+        page: 1,
+        limit: 5,
+        sort: "createdAt",
+        direction: "DESC",
+        search: {},
+      },
+    },
+  });
 
   if (device === "mobile") {
     return <Stack> HOMEPAGE MOBILE</Stack>;
